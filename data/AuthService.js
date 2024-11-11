5496d0223ec2b7bc27883aa07b71fec4b3dc265c
gianmarco zodda <zoddagj@gmail.com>
2024-11-11 20:41:11 -0300

    actualizo la version de expo y modifico el login de  authservice para recibir un error en vez de una response al ingresar credenciales incorrectas

--- data/AuthService.js
+++ data/AuthService.js
@@ -3,27 +3,36 @@ import API_URL_BACKEND from "./api/apiUrl"
 import {credencialesIncorrectas, errorDeConexion} from '../utils/strings/strings';
 
 export const login = async (username, password) => {
-    console.log(API_URL_BACKEND)
-    console.log(username)
-    console.log(password)
+  console.log(API_URL_BACKEND);
+  console.log(username);
+  console.log(password);
+
   try {
-    console.log("entro al try del servicio")
-    const response = await axios.post(`${API_URL_BACKEND}login`, { username, password });
-    console.log(response)
-    console.log("-------------")
-    console.log(response.data.token)
-    console.log("-------------")
-    if (response.data.token) {
-        console.log("token: " + response.data.token)
-      return response.data.token;
-      //es exitoso, me devuelve el token
-    } else {
-        console.log("credenciales incorrectas")
-      throw new Error(credencialesIncorrectas);
-      //no es exitoso, algo mal con lo ingresado (me devuelve el mensaje predefinido)
-    }
+      console.log("entro al try del servicio");
+      const response = await axios.post(`${API_URL_BACKEND}login`, { username, password });
+      console.log("response: ", response);
+
+      if (response.data && response.data.token) {
+          console.log("token: ", response.data.token);
+          return response.data.token;
+      } else {
+          console.log("credenciales incorrectas");
+          throw new Error('Credenciales incorrectas');
+      }
   } catch (error) {
-    console.log(errorDeConexion)
-    throw new Error(errorDeConexion);
+      if (error.response) {
+          // Si el error tiene respuesta, lo que significa que es un error HTTP
+          console.log("Error en la respuesta del servidor: ", error.response.data);
+          throw new Error(error.response.data.message || 'Error de conexión');
+      } else if (error.request) {
+          // Si no hay respuesta (por ejemplo, la solicitud no se envió correctamente)
+          console.log("Error en la solicitud: ", error.request);
+          throw new Error('Error de conexión');
+      } else {
+          // Cualquier otro error no relacionado con la respuesta HTTP
+          console.log("Error desconocido: ", error.message);
+          throw new Error('Error desconocido');
+      }
   }
 };
+
--- data/api/apiUrl.js
+++ data/api/apiUrl.js
@@ -1,3 +1,3 @@
-const API_URL_BACKEND = "http://<IP_DE_TU_CELU>/api/"
+const API_URL_BACKEND = "http://LA_API_DE_TU_COMPU:3000/api/"
 /*si usas el celu, usar en vez de local host, usa la ip de la maquina en la q se levanta el servidor*/
 export default API_URL_BACKEND;
