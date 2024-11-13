b287133f6f02cd6efc94511f597968bb4f94c2ea
gianmarco zodda <zoddagj@gmail.com>
2024-11-13 15:58:30 -0300

    modifico la estructura y datos menores en app.js antes de mergear con la rama settings screen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import LoginScreen from "./presentation/screens/LoginScreen";
 import RegisterScreen from "./presentation/screens/RegisterScreen";
-import SubirScreen from "./presentation/screens/SubirImagenScreen";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import { ThemeProvider } from './data/ThemeContext'
@@ -8,7 +7,6 @@ import React from 'react';
 import BottomTabNavigator from "./presentation/components/BottomBar"
 import { withThemeContainer } from "./presentation/hooks/HookContainer";
 import { AuthProvider } from "./data/AuthContext";
-import ProfileScreen from "./presentation/screens/ProfileScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -18,12 +16,9 @@ export default function App() {
     <ThemeProvider>
       <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Perfil" screenOptions={{ headerShown: false }}>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={withThemeContainer(LoginScreen)} />
         <Stack.Screen name="Register" component={withThemeContainer(RegisterScreen)} />
-        <Stack.Screen name="SubirImagen" component={withThemeContainer(SubirScreen)} />
-        <Stack.Screen name="Perfil" component={withThemeContainer(ProfileScreen)} />
-
         <Stack.Screen
             name="Main"
             component={BottomTabNavigator}
