5e82b57c34d4d214b1d5ec46e9c2f365554766c6
juanMotok32 <juan.motok@gmail.com>
2024-11-18 18:05:32 -0300

    Verificacion de Retina

--- presentation/screens/ProfileScreen.js
+++ presentation/screens/ProfileScreen.js
@@ -15,7 +15,6 @@ const ProfileScreen = ({imagen, fecha, horario}) => {
   const route = useRoute();
 
   const [profile, setProfile] = useState({ 
-    username: 'Juan Motok', 
     profilePic: require('../../assets/persona.jpg') 
   });
   const [miniCards, setMiniCards] = useState([
@@ -27,7 +26,7 @@ const ProfileScreen = ({imagen, fecha, horario}) => {
   useEffect(() => {
     if (route.params?.imagen) {
       const nuevaImagen = {
-        id: `${miniCards.length + 1}`, // Genera un ID único
+        id: `${miniCards.length + 1}`, 
         miniatura: { uri: route.params.imagen },
         fecha: route.params.fecha,
         horario: route.params.horario,
